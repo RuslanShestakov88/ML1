57295f8b3d7bab62c04538a6b36925dcf7c52cc3
RuslanShestakov88 <rshestakov88@gmail.com>
2021-09-24 00:32:59 +0300

    fit (1 class only)

--- downloade.py
+++ downloade.py
@@ -1,3 +1,5 @@
 from bing_image_downloader import downloader
 
 downloader.download("traffic cone", limit=200, output_dir='images')
+
+#downloader.download("sevety vest", limit=200, output_dir='images')
