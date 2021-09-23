dff2e6aa9377a319bcdc8671b8af3a331c851b31
RuslanShestakov88 <rshestakov88@gmail.com>
2021-09-23 23:35:38 +0300

    flatten

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from skimage.io import imread
 from skimage.transform import resize
 
 target = []
-image = []
+images = []
 flat_data = []
 
 DATADIR = 'images/'
@@ -16,5 +16,21 @@ for category in CATEGORIES:
     path = os.path.join(DATADIR, category)
     for img in os.listdir(path):
         img_array = imread(os.path.join(path, img))
-        print(img_array)
+        #print(img_array)
+        #print(img_array.shape)
+        #img_resized = resize(img_array(150,150,3))
+        #flat_data.append(img_resized.flatten())
+        flat_data.append(img_array.flatten())
+        images.append(img_array)
+        target.append(class_num)
 
+flat_data = np.array(flat_data)
+target = np.array(target)
+images = np.array(images)
+
+print(flat_data[0])
+
+a = np.array([[1,2,3,4,5],
+              [4,5,6,7,8]])
+a.ndim
+print(a.flatten())
